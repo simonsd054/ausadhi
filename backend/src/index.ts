378bef548716c1bf4d9c308bf8e66059fbe00867
Simon Dahal <simonsd054@gmail.com>
2024-07-08 19:17:10 +1000

    Delete workflow and fix path for frontend dist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(errorHandler)
 
 app.use(express.static(path.resolve(__dirname, "..", "dist")))
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"))
+  res.sendFile(path.join(__dirname, "..", "dist/index.html"))
 })
 
 app.listen(port, () => {
